d384794308ae8591a1259d21b7ca02e2cb611e96
nlxn <nlxn@proton.me>
2026-05-01 13:31:34 +0300

    Add files via upload

---
+++ data/config.js
@@ -0,0 +1,57 @@
+// config.js
+
+export const meta = {
+  title: 'Nelson Nathan - NLXN™ /ˈnɛl.sən/',
+  description: 'From pixels to code, I design and build digital experiences that look great and work seamlessly. Nelson Nathan – where creativity and technology meet.'
+};
+
+export const hero = {
+  title: "Howdy! 🤠 I'm Nelson Nathan.",
+  desc: "A multidisciplinary designer & developer. NLXN™ /ˈnɛl.sən/"
+};
+
+export const projects = {
+  title: 'My Links',
+  desc: "Here are links to my portfolio, and socials.",
+  projects: [
+    {
+      title: 'NLXN™',
+      description: 'Nelson :)',
+      link: 'https://nlxn.xyz',
+      icon: '/static/icons/nlxn.svg'
+    },
+    {
+      title: 'GitHub',
+      description: 'Coding stuff!',
+      github: 'NLXN'
+    },
+    {
+      title: 'Behance',
+      description: 'I\'m just a Hueman!',
+      behance: 'NLXN'
+    },
+    {
+      title: 'X',
+      description: 'Not videos!',
+      x: '1NLXN'
+    },
+  ]
+};
+
+export const stack = {
+  title: 'My Stack',
+  stack: [
+    'HTML',
+    'CSS',
+    'JavaScript',
+    'React',
+    'Next.js',
+    'Illustrator',
+    'Photoshop'
+  ]
+};
+
+export const contact = {
+  title: 'Contact Me',
+  email: 'nelsonnathan@tuta.io'
+};
